9d5cfbd5e41ed66d49b3faf9ddeb03640ca11327
dragosgit96 <114982429+dragosgit96@users.noreply.github.com>
2024-04-27 20:57:00 +0300

    save image in db

--- app/components/myCameraPreview.tsx
+++ app/components/myCameraPreview.tsx
@@ -5,18 +5,47 @@ import {
   View,
   TouchableOpacity,
   ImageBackground,
+  Alert,
 } from "react-native";
 import { Stack } from "expo-router";
+import { storage } from "../../firebase.config";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 
-const MyCameraPreview = ({ onExitPreview, photo }) => {
+const MyCameraPreview = ({ onExitPreview, image }) => {
   const __closeCameraPreview = () => {
     onExitPreview();
+  };
+  const imageFolderPath = "images/";
+
+  const saveImage = () => {
+    if (image) {
+      const imageName = image.uri.slice(-16, -4);
+      uploadImage(image.uri, imageName)
+        .then((downloadURL) => {
+          Alert.alert("Image saved.");
+          console.log("Image saved. URL:", downloadURL);
+        })
+        .catch((error) => {
+          Alert.alert("Could now save image.");
+          console.log(error);
+        });
+    }
+  };
+
+  const uploadImage = async (uri, name) => {
+    const respone = await fetch(uri);
+    const blob = await respone.blob();
+    const imageRef = ref(storage, imageFolderPath + name);
+    await uploadBytes(imageRef, blob);
+    const downloadURL = await getDownloadURL(imageRef);
+
+    return downloadURL;
   };
 
   return (
     <View style={styles.container}>
       <ImageBackground
-        source={{ uri: photo && photo.uri }}
+        source={{ uri: image && image.uri }}
         style={{
           flex: 1,
         }}
@@ -26,6 +55,10 @@ const MyCameraPreview = ({ onExitPreview, photo }) => {
         style={styles.closeButton}
       >
         <Text style={styles.buttonText}>X</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={saveImage} style={styles.saveButton}>
+        <Text style={styles.buttonText}>Save</Text>
       </TouchableOpacity>
     </View>
   );
@@ -39,10 +72,14 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     position: "relative",
   },
-
   closeButton: {
     position: "absolute",
     top: "5%",
+    right: "5%",
+  },
+  saveButton: {
+    position: "absolute",
+    bottom: "5%",
     right: "5%",
   },
   buttonText: {
