b442447513e74bb3c59cdc12838fde5cd65d1d2b
dragosgit96 <114982429+dragosgit96@users.noreply.github.com>
2024-05-28 17:08:16 +0300

    changed tabs names

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,8 +3,14 @@ import { Tabs } from "expo-router";
 export default () => {
   return (
     <Tabs>
-      <Tabs.Screen name="home" options={{ headerShown: false }} />
-      <Tabs.Screen name="myCamera" options={{ headerShown: false }} />
+      <Tabs.Screen
+        name="home"
+        options={{ headerShown: false, title: "Home" }}
+      />
+      <Tabs.Screen
+        name="myCamera"
+        options={{ headerShown: false, title: "Camera" }}
+      />
     </Tabs>
   );
 };
